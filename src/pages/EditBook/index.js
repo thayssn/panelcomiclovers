3b57cd0332aff31734f158b20c8694138c0f6056
Thays <thays.sn@hotmail.com>
2019-08-25 16:48:45 -0300

    book editing

---
+++ src/pages/EditBook/index.js
@@ -0,0 +1,300 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import EditBookContainer from './style';
+import env from '../../env';
+// import './New.scss';
+const ReactTags = require('react-tag-autocomplete');
+
+class EditBook extends Component {
+  state = {
+    originalBook: {},
+    preview: null,
+    image: null,
+    isbn: '',
+    title: '',
+    description: '',
+    edition: '',
+    price: '',
+    pages: 0,
+    publishing_date: '',
+    format: '',
+    illustrators: [],
+    writers: [],
+    licensors: [],
+    publishers: [],
+    selectedIllustrators: [],
+    selectedWriters: [],
+    selectedLicensors: [],
+    selectedPublishers: [],
+  };
+
+  async componentWillMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const { data: book } = await api.get(`books/${id}`);
+    this.setState({
+      originalBook: book,
+      ...book,
+      selectedIllustrators: book.illustrators,
+      selectedWriters: book.writers,
+      selectedLicensors: book.licensors,
+      selectedPublishers: book.publishers,
+      preview: `${env.baseUrl}/${book.thumbnail}`,
+    });
+  }
+
+  async componentDidMount() {
+    const { data: illustrators } = await api.get('illustrators');
+    this.setState({ illustrators });
+
+    const { data: publishers } = await api.get('publishers');
+    this.setState({ publishers });
+
+    const { data: writers } = await api.get('writers');
+    this.setState({ writers });
+
+    const { data: licensors } = await api.get('licensors');
+    this.setState({ licensors });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { history, match } = this.props;
+    const { id } = match.params;
+    const data = new FormData();
+
+    const {
+      originalBook,
+      image,
+      isbn,
+      title,
+      description,
+      edition,
+      price,
+      pages,
+      publishing_date,
+      format,
+      selectedIllustrators,
+      selectedWriters,
+      selectedLicensors,
+      selectedPublishers,
+    } = this.state;
+
+    const payload = {
+    };
+
+    if (isbn !== originalBook.isbn) {
+      payload.isbn = isbn;
+    }
+    if (title !== originalBook.title) {
+      payload.title = title;
+    }
+    if (description !== originalBook.description) {
+      payload.description = description;
+    }
+    if (edition !== originalBook.edition) {
+      payload.edition = edition;
+    }
+    if (price !== originalBook.price) {
+      payload.price = price;
+    }
+    if (pages !== originalBook.pages) {
+      payload.pages = pages;
+    }
+    if (publishing_date !== originalBook.publishing_date) {
+      payload.publishing_date = publishing_date || null;
+    }
+    if (format !== originalBook.format) {
+      payload.format = format;
+    }
+
+    payload.illustrators = selectedIllustrators.map(i => i.id);
+    payload.writers = selectedWriters.map(i => i.id);
+    payload.licensors = selectedLicensors.map(i => i.id);
+    payload.publishers = selectedPublishers.map(i => i.id);
+
+    data.append('jsonPayload', JSON.stringify(payload));
+    data.append('image', image);
+
+    try {
+      await api.put(`books/${id}`, data);
+      history.push(`/books/${id}`);
+    } catch (err) {
+      console.log(err);
+      alert('Houve um erro ao atualizar o quadrinho.');
+    }
+  }
+
+  handleFileChange = (e) => {
+    this.setState({ image: e.target.files[0], preview: URL.createObjectURL(e.target.files[0]) });
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleBlur = (e) => {
+    e.target.value = '';
+    this.setState({ [e.target.name]: '' });
+  }
+
+  handleListChange = (currentThing, currentValue) => {
+    this.setState(state => ({ ...state, [currentThing]: [...state[currentThing], currentValue] }));
+  }
+
+  handleDelete = (i, key) => {
+    const { [key]: currentKey } = this.state;
+    const selectedValues = currentKey.slice(0);
+    selectedValues.splice(i, 1);
+    this.setState({ [key]: selectedValues });
+  }
+
+  handleAddition = (tag, key) => {
+    const { [key]: currentKey } = this.state;
+    const selectedValues = [].concat(currentKey, tag);
+    this.setState({ [key]: selectedValues });
+  }
+
+  render() {
+    const {
+      isbn, title, description, edition, preview, price,
+      pages, publishing_date, format,
+      illustrators, writers, licensors, publishers,
+      selectedIllustrators, selectedWriters, selectedLicensors, selectedPublishers,
+    } = this.state;
+
+    return (
+      <EditBookContainer className="new_post">
+        <h1>Cadastrar novo quadrinho</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div>
+            <div className="thumbnail">
+              <img src={preview} alt="" className="thumbnail" />
+            </div>
+            <input
+              className="btn"
+              type="file"
+              onChange={this.handleFileChange}
+            />
+          </div>
+
+          <input
+            type="text"
+            name="isbn"
+            placeholder="ISBN"
+            onChange={this.handleChange}
+            value={isbn}
+            required
+          />
+
+          <input
+            type="text"
+            name="title"
+            placeholder="Título"
+            onChange={this.handleChange}
+            value={title}
+            required
+          />
+
+          <input
+            type="text"
+            name="edition"
+            placeholder="Edição"
+            onChange={this.handleChange}
+            value={edition}
+          />
+
+          <input
+            type="number"
+            name="pages"
+            placeholder="Páginas"
+            onChange={this.handleChange}
+            value={pages}
+          />
+
+          <input
+            type="text"
+            name="description"
+            placeholder="Descrição"
+            onChange={this.handleChange}
+            value={description}
+          />
+
+          <input
+            type="date"
+            name="publishing_date"
+            placeholder="Data de publicação"
+            onChange={this.handleChange}
+            value={publishing_date}
+          />
+
+          <input
+            type="text"
+            name="format"
+            placeholder="Formato"
+            onChange={this.handleChange}
+            value={format}
+          />
+
+          <input
+            type="text"
+            name="price"
+            placeholder="Preço. Ex: 13.23"
+            onChange={this.handleChange}
+            value={price}
+          />
+
+
+          <ReactTags
+            maxSuggestionsLength={50}
+            tags={selectedIllustrators}
+            suggestions={illustrators}
+            handleDelete={i => this.handleDelete(i, 'selectedIllustrators')}
+            handleAddition={tag => this.handleAddition(tag, 'selectedIllustrators')}
+            autoresize={false}
+            minQueryLength={0}
+            placeholder="Adicionar ilustrador"
+          />
+
+          <ReactTags
+            maxSuggestionsLength={50}
+            tags={selectedWriters}
+            suggestions={writers}
+            handleDelete={i => this.handleDelete(i, 'selectedWriters')}
+            handleAddition={tag => this.handleAddition(tag, 'selectedWriters')}
+            autoresize={false}
+            minQueryLength={0}
+            placeholder="Adicionar escritor"
+          />
+
+          <ReactTags
+            maxSuggestionsLength={50}
+            tags={selectedLicensors}
+            suggestions={licensors}
+            handleDelete={i => this.handleDelete(i, 'selectedLicensors')}
+            handleAddition={tag => this.handleAddition(tag, 'selectedLicensors')}
+            autoresize={false}
+            minQueryLength={0}
+            placeholder="Adicionar licenciante"
+          />
+
+          <ReactTags
+            maxSuggestionsLength={50}
+            tags={selectedPublishers}
+            suggestions={publishers}
+            handleDelete={i => this.handleDelete(i, 'selectedPublishers')}
+            handleAddition={tag => this.handleAddition(tag, 'selectedPublishers')}
+            autoresize={false}
+            minQueryLength={0}
+            placeholder="Adicionar editora"
+          />
+
+          <button type="submit" className="submit">Enviar</button>
+        </form>
+      </EditBookContainer>
+    );
+  }
+}
+
+export default EditBook;
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import CreateUser from './pages/CreateUser';
 import Books from './pages/Books';
 import Book from './pages/Book';
 import CreateBook from './pages/CreateBook';
+import EditBook from './pages/EditBook';
 import Illustrators from './pages/Illustrators';
 import CreateIllustrator from './pages/CreateIllustrator';
 import Licensors from './pages/Licensors';
@@ -22,21 +23,22 @@ function Routes() {
       <Route exact path="/login" component={Login} />
 
       <Route exact path="/users" component={Users} />
-      <Route path="/users/create" component={CreateUser} />
+      <Route exact path="/users/create" component={CreateUser} />
       {/* <Route path="/users/:id" component={User} /> */}
 
+      <Route exact path="/books/create" component={CreateBook} />
+      <Route exact path="/books/:id" component={Book} />
+      <Route exact path="/books/:id/edit" component={EditBook} />
       <Route exact path="/books" component={Books} />
-      <Route path="/books/create" component={CreateBook} />
-      <Route path="/books/:id" component={Book} />
 
       <Route exact path="/writers" component={Writers} />
-      <Route path="/writers/create" component={CreateWriter} />
+      <Route exact path="/writers/create" component={CreateWriter} />
       <Route exact path="/licensors" component={Licensors} />
-      <Route path="/licensors/create" component={CreateLicensor} />
+      <Route exact path="/licensors/create" component={CreateLicensor} />
       <Route exact path="/publishers" component={Publishers} />
-      <Route path="/publishers/create" component={CreatePublisher} />
+      <Route exact path="/publishers/create" component={CreatePublisher} />
       <Route exact path="/illustrators" component={Illustrators} />
-      <Route path="/illustrators/create" component={CreateIllustrator} />
+      <Route exact path="/illustrators/create" component={CreateIllustrator} />
     </Switch>
   );
 }
